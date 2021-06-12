25d96a6ddef1fbf22b42df9da5cf520d1072d1d5
Daniel Hedrick <misterhedrick@gmail.com>
2021-06-12 09:36:37 -0400

    register sw

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-    .register('../../ngsw-worker.js')
+    .register('/ngsw-worker.js')
     .then(function() {
         console.log('Service Worker Registered')
     })
